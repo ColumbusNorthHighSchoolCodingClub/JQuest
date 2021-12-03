1e3b2aaca1678a9bf4a832c4208ff69f21c425e5
Rishi Rao <22raor@gmail.com>
2021-12-03 07:40:35 -0500

    Add conversion

--- src/util/JsonManager.java
+++ src/util/JsonManager.java
@@ -1,5 +1,26 @@
 package util;
 
-public class JsonManager {
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.json.JSONObject;
+
+public class JsonManager {
+	
+	public static Map<String,String> getMap(String data) {
+		var j = new JSONObject(data);
+		var map = j.toMap();
+		
+		var newMap = map.entrySet().stream()
+			     .collect(Collectors.toMap(Map.Entry::getKey, e -> (String)e.getValue()));
+		
+		return newMap;
+	}
+	
+	public static String getJSONString(Map<String,String> data) {
+		var j = new JSONObject(data);
+		return j.toString();
+	}
+	
+	
 }
