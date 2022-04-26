648714b4939d8d8caab960003c98eef35883cc6b
Carlos E Fabian <96143707+Carlos-here@users.noreply.github.com>
2022-04-26 15:07:31 -0400

    Updating Map

--- src/game/player/Map.java
+++ src/game/player/Map.java
@@ -18,15 +18,10 @@ import misc.ImagePanel;
 public class Map
 {
 	private Image ffbackground;
-<<<<<<< HEAD
         private Image sfbackground;//Uses image of school layout
         
         public ArrayList<String> availableStations;
-=======
-    private Image sfbackground;
-    public ArrayList<String> availableStations;
     
->>>>>>> 23e8b6902cf8b45ec102901ea00f12de2be08b84
 	public Map(ArrayList<Integer> availableFFStations,ArrayList<Integer> availableSFStations,String currentStation,String currentDestination,String currentTimeline) throws FileNotFoundException
 	{
 		ffbackground = new Image(new FileInputStream("src\\images\\FirstFloor_Reference.png"));
@@ -90,84 +85,6 @@ public class Map
     public void TransferSFStations(ArrayList<Integer> availableSFStations)
     {
         
-<<<<<<< HEAD
-        public Image plantSFStations(Image xx, ArrayList<Integer> list)
-        {
-            WritableImage result = new WritableImage(sfbackground.getPixelReader(),(int)sfbackground.getWidth(),(int) sfbackground.getHeight());  //Creates a copy image that can be edited
-            PixelWriter editor = result.getPixelWriter(); // creates an object that can edit the image 
-            for(int x=30; x<40;x++)
-                for(int y=30; y<40;y++)
-                    editor.setColor(x, y, Color.CORAL);
-            return result;
-        }
-        
-        public void TransferFFStations(ArrayList<Integer> availableFFStations)
-        {
-            
-            for(Integer i : availableFFStations) // loops through the first floor stations to create a list of the locautons of all the stations
-                switch (i)
-                {
-                    case 1:
-                        availableStations.add("Location");
-                        continue;
-                    case 2:
-                        availableStations.add("Location");
-                        continue;
-                    case 3:
-                        availableStations.add("Location");
-                        continue;
-                    case 4:
-                        availableStations.add("Location");
-                        continue;
-                    case 5:
-                        availableStations.add("Location");
-                        continue;
-                    case 6:
-                        availableStations.add("Location");
-                        continue;
-                    case 7:
-                        availableStations.add("Location");
-                        continue;
-                    case 8:
-                        availableStations.add("Location");
-                        continue;
-                }
-        }
-        
-        public void TransferSFStations(ArrayList<Integer> availableSFStations)
-        {
-            
-            for(Integer i : availableSFStations)// loops through the second floor stations to create a list of the locautons of all the stations
-                switch (i)
-                {
-                    case 1:
-                        availableStations.add("Location");
-                        continue;
-                    case 2:
-                        availableStations.add("Location");
-                        continue;
-                    case 3:
-                        availableStations.add("Location");
-                        continue;
-                    case 4:
-                        availableStations.add("Location");
-                        continue;
-                    case 5:
-                        availableStations.add("Location");
-                        continue;
-                    case 6:
-                        availableStations.add("Location");
-                        continue;
-                    case 7:
-                        availableStations.add("Location");
-                        continue;
-                    case 8:
-                        availableStations.add("Location");
-                        continue;
-                }
-        }
-            
-=======
         for(Integer i : availableSFStations)
             switch (i)
             {
@@ -197,5 +114,4 @@ public class Map
                     continue;
             }
     }
->>>>>>> 23e8b6902cf8b45ec102901ea00f12de2be08b84
 }
